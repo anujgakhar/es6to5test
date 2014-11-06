be0d4ad9376775f5abe10f63fb4ef1bc35e3660b
Anuj Gakhar <anuj.gakhar@gmail.com>
2014-11-06 16:46:00 +0000

    initial commit

---
+++ cat.js
@@ -0,0 +1,24 @@
+class Cat {
+ 
+  constructor(name='Simon') {
+    this.name = name
+  }
+ 
+  meow() {
+    return 'MROW'
+  }
+ 
+  destroyTheHouse() {
+    window['Object'] = undefined
+  }
+ 
+  eat() {
+    return 'nom nom nom'
+  }
+ 
+  sleep(callback) {
+    setTimeout(callback, 1000)
+  }
+}
+
+module.exports = Cat
---
+++ test/test-cat.js
@@ -0,0 +1,25 @@
+var Cat = require('../cat5');
+var assert = require('assert');
+
+describe('test cat', function () {
+	it('should exist', function () {
+		var cat = new Cat();
+		assert.equal(typeof cat === 'object', true)
+	})
+
+	it('should have a default name of Simon', function () {
+		var cat = new Cat();
+		assert.equal(cat.name === 'Simon', true);
+	})
+
+	it('should have a name if given a name', function () {
+		var cat = new Cat('Johnny');
+		assert.equal(cat.name === 'Johnny', true);
+	})
+
+	it('should meow', function () {
+		var cat = new Cat();
+		var meow = cat.meow();
+		assert.equal(meow, 'MROW');
+	})	
+})
